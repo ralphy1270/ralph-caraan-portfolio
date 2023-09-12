218d8bc474f44b63cd220772e28cbc0be9974fc0
caraanralphy <caraanralph@gmail.com>
2023-09-12 21:01:13 +0800

    Ralphy na

--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = (prop) => {
                   isClicked && "text-white lg:text-black"
                 } text-lg font-semibold dark:font-medium`}
               >
-                Ralph Albert Caraan
+                Ralphy Caraan
               </div>
               <span dangerouslySetInnerHTML={{ __html: svgCode }}></span>
             </div>
